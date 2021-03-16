56fbac6a3c897de8e0a7b09b104aa8997c21d702
Rinat <50962224+Rinaldini@users.noreply.github.com>
2021-03-16 23:21:46 +0500

    Update Converter.java
    Читает поток данных JSON, почти определяет выражение для единиц измерения

--- Converter.java
+++ Converter.java
@@ -4,17 +4,17 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-import java.io.FileNotFoundException;
-
 import java.io.ByteArrayOutputStream;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
 
-import org.json.simple.JSONArray;
+//import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -22,8 +22,9 @@ import com.opencsv.CSVReader;
 
 public class Converter {
  
-  private static final String file = "C:/Mydocs/Java/universal-converter/src/main/resources/file.csv"; // заменить на args[0]
+  private static final String file = "D:\\Mydoc\\IT\\converter\\src\\main\\resources\\file.csv"; //  заменить на args[0]
   public static String to, from, response;
+  public static int codeResponse;
   public static FileReader reader;
   public static ByteArrayOutputStream result = new ByteArrayOutputStream();
 
@@ -31,13 +32,13 @@ public class Converter {
 
     HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
     server.createContext("/convert", new ConvertHandler());
-    server.setExecutor(null); // creates a default executor
+    server.setExecutor(null);
     server.start();
  
   }
 
  /**
-  *  считываем данные из файла *.csv 
+  *   считываем данные из файла *.csv
   */
 
   public static void readData(String csvFile) {
@@ -47,11 +48,9 @@ public class Converter {
 
       String[] nextLine;
       while ((nextLine = csvReader.readNext()) != null) {
-        for (String cell : nextLine) {
-          System.out.print(cell + "\t");
-        }
-        System.out.println();
+          System.out.print(nextLine[0] +  " " + nextLine[1] + " " + nextLine[2] + "\r\n");
       }
+      System.out.println(); // удалить перед сдачей
     }
     catch (Exception e) {
       e.printStackTrace();
@@ -70,31 +69,41 @@ public class Converter {
           while ((length = exchange.getRequestBody().read(buffer)) != -1) {
             result.write(buffer, 0, length);
           }
-          System.out.println(result.toString("UTF-8"));
+          System.out.println(result.toString(StandardCharsets.UTF_8)); // удалить перед сдачей
         } catch (Exception e) {
           e.printStackTrace();
         }
-        // считывание файла с JSON данными и последующий разбор 
+        // считывание потока с JSON данными и последующий разбор
         try {
           JSONParser jsonParser = new JSONParser();
-          JSONObject jsonObject = (JSONObject) jsonParser.parse(result.toString("UTF-8"));
+          JSONObject jsonObject = (JSONObject) jsonParser.parse(result.toString(StandardCharsets.UTF_8));
           from = ((String) jsonObject.get("from")).replaceAll(" ","");
           to = ((String) jsonObject.get("to")).replaceAll(" ","");
-        } catch (IOException e) {
-          e.printStackTrace();
-        } catch (ParseException e) {
-          e.printStackTrace();
-        } catch (NullPointerException e) {
+          result.reset();
+          result.close();
+        } catch (ParseException | NullPointerException e) {
           e.printStackTrace();
         }
         System.out.println("The TO is: " + to + " and FROM is: " + from);  // удалить перед сдачей
        
       } else {
-        response = "This is the GET response";
-      } 
+        response = "This is the GET response"; // удалить перед сдачей
+      }
+      //Pattern p = Pattern.compile("\\w/\\w");
+      //Matcher m = p.matcher(from);
+
+      if (from.equals("")) {
+        codeResponse = 400;
+      } else if (from.matches("\\w+\\s*/\\w+\\s*")) {
+        codeResponse = 404;
+        System.out.println("УРА!!! Видим единицы измерения: " + from);
+      } else {
+        codeResponse = 200;
+      }
+
 
       readData(file);      
-      exchange.sendResponseHeaders(200, response.length());
+      exchange.sendResponseHeaders(codeResponse, response.length());
       OutputStream os = exchange.getResponseBody();
       os.write(response.getBytes());
       os.flush();
